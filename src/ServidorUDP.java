df78b49bb4cf7c07062803876fe18dbe4bf51f9d
Danielfib <dfb2@cin.ufpe.br>
2017-06-30 12:00:57 -0300

    oi

--- src/ServidorUDP.java
+++ src/ServidorUDP.java
@@ -9,20 +9,31 @@ public class ServidorUDP {
 	public static void main(String[] args) {
 		try {
 			DatagramSocket serverSocket = new DatagramSocket(2020);
-			byte[] dados = new byte[200];
+			byte[] dados = new byte[400];
 			DatagramPacket pacote = new DatagramPacket(dados, dados.length);
 			while(true) {
+				System.out.println("1");
 				serverSocket.receive(pacote);
-				ByteArrayInputStream bao = new ByteArrayInputStream(pacote.getData());
-                ObjectInputStream ous = new ObjectInputStream(bao);
+				System.out.println("2");
+				ByteArrayInputStream bao = new ByteArrayInputStream(pacote.getData());                
+				ObjectInputStream ous = new ObjectInputStream(bao);
+				System.out.println("3");
                 Pacote p = (Pacote) ous.readObject();
+                System.out.println("4");
+                System.out.println(p.syn);
+               
+//                System.out.println(pacote.getAddress());
+//                System.out.println(pacote.getSocketAddress());
                 
+                int server_isn = -2;
                 if(p.syn) {
-                	if(p.numSeq == 7) {
-                		//primeira via e envia a segunda
+                	if(server_isn == -1) {
+                		//primeira via e envia a segunda                		
                 	} else {
-                		//terceira via
+                		//envia terceira via
                 	}
+                } else {
+                	//recebeu 3 via
                 }
 			}
 		} catch (IOException | ClassNotFoundException e) {
