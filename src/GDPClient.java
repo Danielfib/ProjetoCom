469cc0f387eead3128263ddf24fc89d57167e391
Mateusnunes <mateusnunes@me.com>
2017-07-01 12:17:52 -0300

    Atualizando

--- src/GDPClient.java
+++ src/GDPClient.java
@@ -1,11 +1,6 @@
 
 //funções que n podem estar direto no cliente pq o cliente tem que estar ouvindo a Interface
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 public class GDPClient {
@@ -18,34 +13,9 @@ public class GDPClient {
 		this.sendBase = sendBase;
 		this.nextSeqNum = nextSeqNum;
 		this.state = state;
-		clientSocket = new DatagramSocket();
-	}
-	
-	//apresentação de 3-vias
-	public boolean startConection() {
-		Pacote p = new Pacote(0, 0, 7, -1, false, false, true, false, 0, 0, "Teste".getBytes());
-		try {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(p);
-			oos.close();
-			byte msgTcp [] = baos.toByteArray();
-			
-			InetAddress ip = InetAddress.getByName("localhost");
-			DatagramPacket pkt = new DatagramPacket(msgTcp, msgTcp.length, ip, 2020);
-
-			clientSocket.send(pkt);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return false;
+		clientSocket = new DatagramSocket(2021);
 	}
 
-	public void GDPsend(Pacote enviar, byte[] msg, InetAddress addr, int porta) throws SocketException {
-		
+	public void GDPsend(Pacote pacote, String addr, int porta) {
 	}
-	//falta timeout
-	
-
 }
